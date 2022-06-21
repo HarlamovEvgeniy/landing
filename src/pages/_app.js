9984d6a75c0da28d8bceb3832ebf3b4737408414
HarlamovEvgeniy <harlamov.mail@mail.ru>
2022-06-21 11:35:16 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import Router from 'next/router';
 // Load DM Sans typeface
 import 'typeface-dm-sans';
+import React from 'react';
 
 // Load other package css file
 import 'react-multi-carousel/lib/styles.css';
@@ -9,11 +10,5 @@ import 'react-modal-video/css/modal-video.min.css';
 import 'rc-drawer/assets/index.css';
 
 export default function CustomApp({ Component, pageProps }) {
-  useEffect(() => {
-    initGA();
-    logPageView();
-    Router.events.on('routeChangeComplete', logPageView);
-  }, []);
-
   return <Component {...pageProps} />;
 }
